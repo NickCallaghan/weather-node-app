e6d3255bfeca0c5dfd8e0e8a35e44eca930a64cf
Nick Callaghan <nicholas.callaghan@gmail.com>
2019-08-13 22:38:08 +0100

    intial commit

---
+++ weather.js
@@ -0,0 +1,47 @@
+const http = require('http');
+const cliArguments = process.argv.slice(2);
+
+// This app users the open weather api, please sign up for an api key at www.openweathermap.org
+const apiKey = require('./apiKey.json');
+
+
+function location(cliArguments) {
+    const location = `${cliArguments[0]},${cliArguments[1]}`;
+    return location
+}
+
+function printWeather(weather) {
+    console.log(`The weather in ${weather.name}, ${weather.sys.country} is: ${weather.weather[0].description} and current temparature is ${weather.main.temp} kelvin `);
+}
+
+function printError(messgage) {
+
+}
+
+function get(apiKey, location) {
+
+    const queryString = `http://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=${apiKey}`;
+
+    try {
+        const request = http.get(queryString, res => {
+            if (res.statusCode === 200) {
+                let body = "";
+                // Read data
+                res.on('data', chunk => {
+                    body += chunk;
+                });
+                res.on('end', () => {
+                    const weather = JSON.parse(body);
+                    printWeather(weather);
+                })
+            } else {
+                // handle error
+            }
+        });
+
+    } catch (err) {
+        console.log(err.messgage);
+    }
+}
+
+get(apiKey.key, location(cliArguments));
