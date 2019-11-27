b55ca514cc77f08975e31bbda25aad35af771422
Nick Callaghan <nicholas.callaghan@gmail.com>
2019-11-27 06:36:51 +0000

    API set up as envirment variable

--- weather.js
+++ weather.js
@@ -1,48 +1,51 @@
-const http = require('http');
+const http = require("http");
 const cliArguments = process.argv.slice(2);
+require("dotenv").config();
 
 // This app users the open weather api, please sign up for an api key at www.openweathermap.org
-const apiKey = require('./apiKey.json');
-
+const apiKey = process.env.API_KEY;
 
 function location(cliArguments) {
-    const location = `${cliArguments[0]},${cliArguments[1]}`;
-    return location
+  const location = `${cliArguments[0]},${cliArguments[1]}`;
+  return location;
 }
 
 function printWeather(weather) {
-    console.log(`The weather in ${weather.name}, ${weather.sys.country} is: ${weather.weather[0].description} and current temparature is ${weather.main.temp} kelvin `);
+  console.log(
+    `The weather in ${weather.name}, ${weather.sys.country} is: ${weather.weather[0].description} clear and the windspeed is ${weather.wind.speed}mps`
+  );
 }
 
 function printError(error) {
-    console.error(error.message);
+  console.error(error.message);
 }
 
 function get(apiKey, location) {
-
-    const queryString = `http://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=${apiKey}`;
-
-    try {
-        const request = http.get(queryString, res => {
-            if (res.statusCode === 200) {
-                let body = "";
-                // Read data
-                res.on('data', chunk => {
-                    body += chunk;
-                });
-                res.on('end', () => {
-                    const weather = JSON.parse(body);
-                    printWeather(weather);
-                })
-            } else {
-                const statusCodeError = new Error(`There was an error getting the message for: ${location}. (${http.STATUS_CODES[res.statusCode]})`);
-                printError(statusCodeError)
-            }
+  const queryString = `http://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=${apiKey}`;
+  try {
+    const request = http.get(queryString, res => {
+      if (res.statusCode === 200) {
+        let body = "";
+        // Read data
+        res.on("data", chunk => {
+          body += chunk;
         });
-
-    } catch (err) {
-        console.log(err.message);
-    }
+        res.on("end", () => {
+          const weather = JSON.parse(body);
+          printWeather(weather);
+        });
+      } else {
+        const statusCodeError = new Error(
+          `There was an error getting the message for: ${location}. (${
+            http.STATUS_CODES[res.statusCode]
+          })`
+        );
+        printError(statusCodeError);
+      }
+    });
+  } catch (err) {
+    console.log(err.message);
+  }
 }
 
-get(apiKey.key, location(cliArguments));
+get(apiKey, location(cliArguments));
